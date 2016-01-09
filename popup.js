f1b40a20122c08c14d06560fc1c19b360f1ebb2f
Sheikh Araf <arafsheikh@rocketmail.com>
2016-01-09 18:53:26 +0530

    Minor fixes

--- popup.js
+++ popup.js
@@ -73,12 +73,12 @@ function login_logout (code) {
 
                 //Send the proper header information along with the request
                 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                http.setRequestHeader("Content-length", params.length);
-                http.setRequestHeader("Connection", "close");
+                //http.setRequestHeader("Content-length", params.length);
+                //http.setRequestHeader("Connection", "close");
 
                 http.onreadystatechange = function() {//Call a function when the state changes.
                     if(http.readyState == 4 && http.status == 200) {
-                        console.log(http.responseText);
+                        console.log("Login success");
                     }
                 }
                 http.send(params);
